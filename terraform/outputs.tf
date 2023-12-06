22a4056c8db1a589d269d8fa5b810cc60725f769
David Omokhodion <davidstone097@gmail.com>
2023-12-06 17:59:52 +0100

    Add action script for ansible

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -3,5 +3,5 @@ output "postgres-ip" {
 }
 
 output "public_ip_address" {
-  value = azurerm_public_ip.this.public.ip_configuration[1].public_ip_address
+  value = azurerm_network_interface.postgres.ip_configuration[1].public_ip_address
 }
